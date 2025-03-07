3b515b6fc65393772735c92c0225861c42d2196b
sbancuz <sbancuz@protonmail.com>
2025-03-07 19:46:00 +0100

    Export funcitons from the module and add entry point to the library

---
+++ fit/__init__.py
@@ -0,0 +1,6 @@
+from fit.csv import export_to_csv, import_from_csv
+from fit.distribution import *
+from fit.elf import ELF
+from fit.stencil import Stencil
+
+from .fit import gdb_injector
---
+++ fit/fit.py
@@ -0,0 +1,17 @@
+from fit.injector import Injector
+
+
+def gdb_injector(
+    bin: str,
+    gdb_path: str = "gdb_multiarc",
+    remote: str | None = None,
+    embeded: bool = True,
+) -> Injector:
+    kwargs = {
+        "gdb_path": gdb_path,
+        "embeded": embeded,
+    }
+    if remote is not None:
+        kwargs["remote"] = remote
+
+    return Injector(bin, "gdb", **kwargs)
