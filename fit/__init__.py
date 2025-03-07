3d8779528024f504b37a1883f4966cb9b13f7ac4
sbancuz <sbancuz@protonmail.com>
2025-03-07 19:47:40 +0100

    Export `Injector`

--- fit/__init__.py
+++ fit/__init__.py
@@ -1,6 +1,7 @@
 from fit.csv import export_to_csv, import_from_csv
 from fit.distribution import *
 from fit.elf import ELF
+from fit.injector import Injector
 from fit.stencil import Stencil
 
 from .fit import gdb_injector
